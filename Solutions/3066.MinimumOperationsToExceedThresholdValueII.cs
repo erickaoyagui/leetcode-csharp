5eec3b91f87daddb8aab181001804aa83bfcd5ff
Erick Aoyagui <erickaoyagui@hotmail.com>
2025-02-18 23:36:20 -0300

    :tada: Initial Commit

---
+++ Solutions/3066.MinimumOperationsToExceedThresholdValueII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class MinimumOperationsToExceedThresholdValueII
+    {
+        // Passes 672/676 testcases
+        // Need to improve performance
+        public int MinOperations(int[] nums, int k)
+        {
+            int counter = 0;
+            List<long> numsList = nums.Select(n => (long)n).ToList();
+
+            while (numsList.Min() < k)
+            {
+                long x = numsList.Min();
+                numsList.Remove(x);
+                long y = numsList.Min();
+                numsList.Remove(y);
+                numsList.Add(CalculateNumber(x, y));
+                counter++;
+            }
+
+            return counter;
+        }
+
+        public long CalculateNumber(long x, long y)
+        {
+            return (Math.Min(x, y) * 2 + Math.Max(x, y));
+        }
+    }
+}
---
+++ Tests/3066.Tests.cs
@@ -0,0 +1,62 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class MinimumOperationsToExceedThresholdValueIITests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            MinimumOperationsToExceedThresholdValueII minimum = new MinimumOperationsToExceedThresholdValueII();
+            int[] nums = [2, 11, 10, 1, 3];
+            int k = 10;
+            int expected = 2;
+
+            // Act
+            int result = minimum.MinOperations(nums, k);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            MinimumOperationsToExceedThresholdValueII minimum = new MinimumOperationsToExceedThresholdValueII();
+            int[] nums = [1, 1, 2, 4, 9];
+            int k = 20;
+            int expected = 4;
+
+            // Act
+            int result = minimum.MinOperations(nums, k);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            MinimumOperationsToExceedThresholdValueII minimum = new MinimumOperationsToExceedThresholdValueII();
+            int[] nums = [999999999, 999999999, 999999999];
+            int k = 1000000000;
+            int expected = 2;
+
+            // Act
+            int result = minimum.MinOperations(nums, k);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+    }
+}
