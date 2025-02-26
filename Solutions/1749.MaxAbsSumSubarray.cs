67991ed56c1c6e3433cae18cac2b1cf670a7be1d
Erick Aoyagui <erickaoyagui@hotmail.com>
2025-02-26 01:02:44 -0300

    :sparkles: Solution for problem 1749

---
+++ Solutions/1749.MaxAbsSumSubarray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class MaxAbsSumSubarraySolution
+    {
+        public int MaxAbsoluteSum(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int highest = 0;
+            int highestPositive = 0;
+            int highestNegative = 0;
+
+            foreach (int num in nums) { 
+                highestPositive = Math.Max(0, num + highestPositive);
+                highestNegative = Math.Min(0, num + highestNegative);
+                highest = Math.Max(highest, Math.Max(highestPositive, -highestNegative));
+            }
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    newHighest = 0;
+            //    sum = 0;
+            //    for (int j = i; j < nums.Length; j++)
+            //    {
+            //        sum += nums[j];
+            //        newHighest = Math.Abs(sum);
+            //        highest = (newHighest > highest) ? newHighest : highest;
+            //    }
+            //}
+
+            return highest;
+        }
+    }
+}
---
+++ Tests/1749.Tests.cs
@@ -0,0 +1,56 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class MaxAbsSumSubarrayTests
+    {
+        readonly MaxAbsSumSubarraySolution solution = new MaxAbsSumSubarraySolution();
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            int[] nums = [1, -3, 2, 3, -4];
+            int expected = 5;
+
+            // Act
+            int result = solution.MaxAbsoluteSum(nums);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            int[] nums = [2, -5, 1, -4, 3, -2];
+            int expected = 8;
+
+            // Act
+            int result = solution.MaxAbsoluteSum(nums);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            int[] nums = [-7, -1, 0, -2, 1, 3, 8, -2, -6, -1, -10, -6, -6, 8, -4, -9, -4, 1, 4, -9];
+            int expected = 44;
+
+            // Act
+            int result = solution.MaxAbsoluteSum(nums);
+
+            // Assert
+            Xunit.Assert.Equal(expected, result);
+        }
+    }
+}
