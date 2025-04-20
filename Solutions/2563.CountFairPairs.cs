dff5b9e7c0070515b10e2dab5b6b0cf84c0d6fbc
Erick Aoyagui <erickaoyagui@hotmail.com>
2025-04-20 01:29:49 -0300

    :feature: Starting to solve problem 2563

---
+++ Solutions/2563.CountFairPairs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class CountFairPairsSolution
+    {
+        public long CountFairPairs(int[] nums, int lower, int upper)
+        {
+            return;
+        }
+    }
+}
---
+++ Tests/2563.Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class CountFairPairsTests
+    {
+        readonly CountFairPairsSolution solution = new CountFairPairsSolution();
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            int[] nums = [0, 1, 7, 4, 4, 5];
+            int lower = 3;
+            int upper = 6;
+            long expected = 6;
+
+            // Act
+            long result = solution.CountFairPairs(nums, lower, upper);
+
+            // Assert
+            Xunit.Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            int[] nums = [1, 7, 9, 2, 5];
+            int lower = 3;
+            int upper = 6;
+            long expected = 6;
+
+            // Act
+            long result = solution.CountFairPairs(nums, lower, upper);
+
+            // Assert
+            Xunit.Assert.Equal(result, expected);
+        }
+    }
+}
